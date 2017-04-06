eea96c44a2e3c0a3a579c814e202f41b7c44009f
Verónica Cordobés <verocordobes@hotmail.com>
2017-04-06 23:00:22 +0200

    Final Commit

---
+++ toyguay_iOS/DatosTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DatosTableViewCell.swift
+//  Aliseda
+//
+//  Created by Verónica Cordobés on 1/3/17.
+//  Copyright © 2017 iphonedroid. All rights reserved.
+//
+
+import UIKit
+
+class DatosTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
